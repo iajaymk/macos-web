1523106596910a9d58a0629066c6a59761e358b7
Ajay Kumar <ajaykumarmkaju@gmail.com>
2025-05-21 21:40:30 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Macos web app
+      </div>
+    </>
+  )
+}
+
+export default App
